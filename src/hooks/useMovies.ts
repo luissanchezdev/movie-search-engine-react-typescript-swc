9edabb1067073aa84052f913b5bf77a297e48e82
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-12-16 00:12:08 -0500

    feature: avoid request with the same term

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,17 +1,22 @@
 import { useState } from 'react'
 import { TypeMovies } from '../constants/types';
 import searchMovies from '../services/movies';
+import { useRef } from 'react';
 
 export function useMovies({ search } : { search : string }) {
   const [movies, setMovies] = useState<TypeMovies[] | null>([])
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>('')
+  const previusSearch = useRef<string | null>(search)
   
   const getMovies = async () => {
+
+    if(previusSearch.current === search) return
 
     try {
       setLoading(true)
       setError(null)
+      previusSearch.current = search
       const newData = await searchMovies(search)
       setMovies(newData)
     } catch(e : any) {
